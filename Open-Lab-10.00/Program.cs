ab8b5b9faa987c366dcc25f215d8b917c8a7e6b7
Milos <61538921+MilosM33@users.noreply.github.com>
2020-11-12 19:18:01 +0100

    Open-Lab-10.02

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -11,13 +11,34 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "Elektrika";
-            Console.WriteLine(LOTR.title);
+            LOTR.SetAuthor("Jano");
+            LOTR.SetNumberOfPages(256);
+            LOTR.SetCategory("Action");
+            LOTR.SetTitle("Elektrika");
+            LOTR.SetCategory("Učebnica");
+            LOTR.GetInfo();
+            
         }
     }
     class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public void SetTitle(string title) => this.title = title;
+
+        public void SetNumberOfPages(int pages) => this.pages = pages;
+
+        public void SetCategory(string category) => this.category = category;
+
+        public void SetAuthor(string author) => this.author = author;
+        
+        public void SetReleaseDate(int releaseDate) => this.releaseDate = releaseDate;
+
+        public void GetInfo() => Console.WriteLine($"{title} is very original book long about {pages} by {author} released at {releaseDate}. Category = {category}");
+        
     }
 }
