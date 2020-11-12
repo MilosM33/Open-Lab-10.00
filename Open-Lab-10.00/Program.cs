f3fde2ff2c3fe887852f516d99728859cd88c612
Milos <61538921+MilosM33@users.noreply.github.com>
2020-11-12 19:45:48 +0100

    Open-Lab-10.05

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -13,6 +13,7 @@ namespace Open_Lab_10._00
             Book LOTR2 = new Book();
             Book LOTR3 = new Book("Elektrika", 256);
             Book HOBIT = new Book("Matematika" , 256,"Ján","Učebnica",2020);
+            HOBIT.Category = Book.categoryList[0];
             LOTR2.GetInfo();
             LOTR3.GetInfo();
             HOBIT.GetInfo();
@@ -21,6 +22,10 @@ namespace Open_Lab_10._00
     }
     class Book
     {
+        /// <summary>
+        /// Return list of available categories.
+        /// </summary>
+        public static List<string> categoryList = new List<string>() { "detské", "romantické", "náučné", "sci-fi", "dobrodružné" };
 
         public Book()
         {
@@ -30,6 +35,7 @@ namespace Open_Lab_10._00
             this.category = "-1";
             this.releaseDate = -1;
         }
+
         public Book(string title, int pages)
         {
             this.title = title;
@@ -38,6 +44,7 @@ namespace Open_Lab_10._00
             this.category = "-1";
             this.releaseDate = -1;
         }
+
         public Book(string title, int pages, string author, string category,int releaseDate)
         {
             this.title = title;
@@ -46,32 +53,47 @@ namespace Open_Lab_10._00
             this.category = category;
             this.releaseDate = releaseDate;
         }
+
         private string title;
         private int pages;
         private string category;
         private string author;
         private int releaseDate;
+        /// <summary>
+        /// Return title of book.
+        /// </summary>
         public string Title
         {
             get => title;
             set => title = value;
         }
+        /// <summary>
+        /// Return number of pages.
+        /// </summary>
         public int Pages
         {
             get => pages;
             set => pages = (value < 0 ? -1 : value);
         }
+
+        /// <summary>
+        /// Return category of book.
+        /// </summary>
         public string Category
         {
-            get => category;
             set => category = value;
         }
-        
+        /// <summary>
+        /// Return author name.
+        /// </summary>
         public string Author
         {
             get => author;
             set => author = value;
         }
+        /// <summary>
+        /// Return release date
+        /// </summary>
         public int ReleaseDate
         {
             get => releaseDate;
